d247d454b764130e500c3f9a6070aa3a85993634
BenjaminLin808 <benjaminlin808@gmail.com>
2023-06-27 23:56:50 -0700

    complete

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,20 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, earum vero
+        facilis aut ducimus voluptas provident tenetur hic maiores cum optio
+        inventore fuga, odit neque alias saepe quidem possimus assumenda.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        voluptatum.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
